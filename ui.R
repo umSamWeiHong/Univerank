3a758c031e7c29c2c02593dbceab2a83d2d52765
brownishmacha <97743422+brownishmacha@users.noreply.github.com>
2022-06-22 20:04:31 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,7 +4,9 @@ library(readr)
 library(DT)
 library(tools)
 
-dataWIE <- read.csv("data\\the.csv")
+#dataWIE <- read.csv("C:/Users/atjar/Downloads/THE.csv")
+qs <- read.csv("C:/Users/atjar/Downloads/QS.csv")
+the <- read.csv("C:/Users/atjar/Downloads/THE.csv")
 
 # Define UI for application that plots features of University Ranking
 ui <- fluidPage(theme = bslib::bs_theme(bootswatch = "superhero"),
@@ -19,24 +21,36 @@ ui <- fluidPage(theme = bslib::bs_theme(bootswatch = "superhero"),
                     
                     h3("Selection panel"),      
                     
+                    #can use this if "year" variable is needed ya
                     # Select variable for year
-                    selectInput(inputId = "year", 
-                                label = "Select a year: ",
-                                choices = c("2017", "2018", "2019", "2020","2021","2022"), 
-                                selected = "2018"),
+                    #selectInput(inputId = "year", 
+                     #           label = "Select a year: ",
+                      #          choices = c("2017", "2018", "2019", "2020","2021","2022"), 
+                       #         selected = "2018"),
                     
-                    selectInput(inputId = "rankingType", 
-                                label = "Do you wish to view QS or THE rankings? : ",
-                                choices = c("QS Ranking", "THE Ranking"), 
-                                selected = "QS Ranking"), 
+                   selectInput(inputId = "qsRanking", 
+                                label = "Do you wish to view QS rankings? : ",
+                                choices = qs$University,
+                                selected = "QS Ranking"),
+                    
+                    selectInput(inputId = "theRanking", 
+                                label = "Do you wish to view THE rankings? : ",
+                                choices = the$name,
+                                selected = "THE Ranking"),
+                    
+                    
                     hr(),
-                   
+                    
+                    
                     # Select variable for world rank
                     sliderInput(inputId = "worldRank", 
                                 label = "World Rank: ",
                                 min = 1, max = 50, 
                                 value = "25"),
                     
+                    
+                    
+                    #you dont have to display this if it doesn't suit your liking ya @sam
                     radioButtons("dist", "Place of study :",
                                  c("Global Institutions" = "norm",
                                    "National Institutions" = "unif"
@@ -50,19 +64,23 @@ ui <- fluidPage(theme = bslib::bs_theme(bootswatch = "superhero"),
                                   value = TRUE),
                                   h5("This section allows the table in Data panel to be displayed or hiden
                                      based on user preference"),
+                   
                     br(),
                     br(),
-          
+                   
+                    
                   ),
                   
                   # Output:
                   mainPanel(
-                    
+
+                    # to be edited based on preference
                     tabsetPanel(id = "tabspanel", type = "tabs",
                                 tabPanel(title = "Base 1", 
                                          br(),
+                                        # plotOutput(outputId = "tester"),
                                          h1("Add your preference")),
-                                
+
                                 tabPanel(title = "Base 2", 
                                          
                                          br(),
@@ -81,3 +99,27 @@ ui <- fluidPage(theme = bslib::bs_theme(bootswatch = "superhero"),
                   )
                 )
 )
+
+# Reference server to display ui(To be modified by sam)
+server <- function(input, output, session) {
+  
+  # Display data table when box is checked
+  observeEvent(input$show_data, {
+    if(input$show_data){
+      showTab(inputId = "tabspanel", target = "Data", select = TRUE)
+    } else {
+      hideTab(inputId = "tabspanel", target = "Data")
+    }
+  })
+  
+ # output$tester<-renderPlot({
+    
+  #})
+    
+
+  
+  
+}
+
+# Create Shiny app object
+shinyApp(ui = ui, server = server )
